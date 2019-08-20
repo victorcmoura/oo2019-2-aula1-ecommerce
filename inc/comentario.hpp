a056c20dd36085e26fef7bb4ac458b4c53da6002
Victor Moura <victor_cmoura@hotmail.com>
2019-08-20 15:23:10 -0300

    Initial commit

---
+++ inc/comentario.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Comentario {
+    private:
+        std::string titulo;
+        std::string autor;
+        std::string texto;
+    public:
+        Comentario();
+        std::string get_titulo();
+        std::string get_autor();
+        std::string get_texto();
+
+        void set_titulo(std::string titulo);
+        void set_autor(std::string autor);
+        void set_texto(std::string texto);        
+};
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include "comentario.hpp"
+
+int main(){
+    Comentario c;
+    c.set_titulo("Meu titulo");
+    std::cout << c.get_titulo() << std::endl;
+    return 0;
+}
